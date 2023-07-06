e7d279f64c8dc41d9f9fb2fba2d08c29ff4a4e61
Xanonymous <trusaidlin@gmail.com>
2023-07-07 04:45:32 +0800

    fix: 🐛 filter-branch should done before file change

--- post-backup.sh
+++ post-backup.sh
@@ -18,9 +18,6 @@ readonly BACKUP_POOL_DIR=/pool
 # Find the latest backup file in the `BACKUP_DIR` directory.
 readonly latest_backup_file=$(find $BACKUP_DIR -type f -exec basename {} \; | sort | tail -n 1 | xargs -I{} find $BACKUP_DIR -name {})
 
-# Clean up all the previous backups in the pool directory.
-rm -rf "${BACKUP_POOL_DIR:?}/"*.tgz
-
 # Copy the latest backup file to the `BACKUP_POOL_DIR` directory.
 cp "$latest_backup_file" $BACKUP_POOL_DIR
 
@@ -30,8 +27,11 @@ readonly FILE_BASENAME=$(basename "$latest_backup_file")
 
 git pull origin main
 
-# Remove the previous backup file from the git repository.
+# Remove the previous backup file from the git repository. (This should be done before any un-staged changes are made.)
 git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *.tgz' --prune-empty --tag-name-filter cat -- --all
+
+# Clean up all the previous backups in the pool directory.
+rm -rf "${BACKUP_POOL_DIR:?}/"*.tgz
 
 git add "."
 git config --global user.email "auto-actions[bot]"
